c080b583540e994bb03bb49e943bc6f4c552e8fc
henricker <henriquevieira@alu.ufc.br>
2022-08-18 14:47:39 -0300

    feat: add default layout

---
+++ src/layouts/style.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const DefaultLayoutContainer = styled.header`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
